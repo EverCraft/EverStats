cc735c40c879eb72226ed66d4d87a637294bed52
rexbut <butbutdu45@gmail.com>
2017-05-05 17:11:00 +0200

    Update : Message

--- src/main/java/fr/evercraft/everstats/ESMessage.java
+++ src/main/java/fr/evercraft/everstats/ESMessage.java
@@ -39,6 +39,7 @@ public class ESMessage extends EMessage<EverStats> {
 	    private final EMessageBuilder french;
 	    private final EMessageBuilder english;
 	    private EMessageFormat message;
+	    private EMessageBuilder builder;
 	    
 	    private ESMessages(final String path, final String french) {   	
 	    	this(path, EMessageFormat.builder().chat(new EFormatString(french), true));
@@ -83,8 +84,13 @@ public class ESMessage extends EMessage<EverStats> {
 			return this.message;
 		}
 		
-		public void set(EMessageFormat message) {
-			this.message = message;
+		public EMessageBuilder getBuilder() {
+			return this.builder;
+		}
+		
+		public void set(EMessageBuilder message) {
+			this.message = message.build();
+			this.builder = message;
 		}
 	}
 }
